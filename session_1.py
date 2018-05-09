6161035c85bef4420b27f2ae3d15942e2213548f
mukki7610 <39070581+mukki7610@users.noreply.github.com>
2018-05-08 20:15:43 -0400

    Add files via upload

---
+++ session_1.py
@@ -0,0 +1,108 @@
+
+# coding: utf-8
+
+# ## Data Types
+
+# ### int
+
+# In[1]:
+
+
+my_int_var=10
+
+
+# In[2]:
+
+
+print(my_int_var)
+
+
+# In[3]:
+
+
+my_int_var_2 = 1000
+
+
+# In[4]:
+
+
+print(my_int_var_2)
+
+
+# In[5]:
+
+
+print(type(my_int_var))
+
+
+# ## float
+
+# In[6]:
+
+
+my_float = 20.2
+
+
+# In[7]:
+
+
+print(my_float)
+
+
+# In[8]:
+
+
+print(type(my_float))
+
+
+# In[9]:
+
+
+type(my_float)
+
+
+# ## string
+
+# In[10]:
+
+
+my_string='session'
+
+
+# In[11]:
+
+
+print(my_string)
+
+
+# In[12]:
+
+
+print(type(my_string))
+
+
+# ## bool
+
+# In[13]:
+
+
+a = 10
+
+
+# In[14]:
+
+
+b = 20
+
+
+# In[15]:
+
+
+c = a>b
+
+
+# In[16]:
+
+
+print(c)
+
